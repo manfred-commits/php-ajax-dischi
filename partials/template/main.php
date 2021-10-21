6446e1a5410d2f2e68de580c02738e85b2efd08e
manfred-commits <manfredipiraino@gmail.com>
2021-10-21 15:30:12 +0200

    added basic albums print, from $db

--- partials/template/main.php
+++ partials/template/main.php
@@ -11,6 +11,14 @@
                         <?php echo $album['title'];?>
                     </h3>
                 </li>
+                <li>
+                    <p>
+                        <?php echo $album['author'];?>
+                    </p>
+                    <p>
+                        <?php echo $album['year'];?>
+                    </p>
+                </li>
             </ul>
         </div>
     <?php }?>

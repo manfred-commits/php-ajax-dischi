b2a354e03754a556303dc229f33cbe10314c5a8e
manfred-commits <manfredipiraino@gmail.com>
2021-10-21 16:08:17 +0200

    added txt container

--- partials/template/main.php
+++ partials/template/main.php
@@ -5,21 +5,24 @@
             <div class="container-album-img">
                 <img src="<?php echo $album['poster']; ?>" alt="">
             </div>
-            <ul>
-                <li>
-                    <h3>
-                        <?php echo $album['title'];?>
-                    </h3>
-                </li>
-                <li>
-                    <p>
-                        <?php echo $album['author'];?>
-                    </p>
-                    <p>
-                        <?php echo $album['year'];?>
-                    </p>
-                </li>
-            </ul>
+            <div class="container-album-txt">
+                <ul>
+                    <li>
+                        <h3>
+                            <?php echo $album['title'];?>
+                        </h3>
+                    </li>
+                    <li>
+                        <p>
+                            <?php echo $album['author'];?>
+                        </p>
+                        <p>
+                            <?php echo $album['year'];?>
+                        </p>
+                    </li>
+                </ul>
+
+            </div>
         </div>
     <?php }?>
 </main>

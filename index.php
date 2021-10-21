3bdc13637290ec6f6928d0ba1ea2758bd439b55f
manfred-commits <manfredipiraino@gmail.com>
2021-10-21 15:26:36 +0200

    added components

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <!-- Prima Milestone:
 Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
+
+<?php 
+
+
+require_once __DIR__ . "/./partials/database.php";
+require_once __DIR__ . "/./partials/vars.php";
+require_once __DIR__ . "/./partials/functions.php";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-ajax-dischi</title>
+</head>
+<body>
+    
+    <?php require_once __DIR__ . "/./partials/template/main.php"; ?>
+
+
+</body>
+</html>

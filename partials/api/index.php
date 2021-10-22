eaede6d5f074b9761b6bbc916d57a979d39c728f
manfred-commits <manfredipiraino@gmail.com>
2021-10-22 14:18:22 +0200

    added condition to filter genres

--- partials/api/index.php
+++ partials/api/index.php
@@ -1,5 +1,19 @@
 <?php
     require __DIR__ . "/../database.php";
     header("Content-Type: application/json");
-    echo json_encode($database);
+
+    if(!empty($_GET['genre'])){
+        $genre=$_GET['genre'];
+        $databaseFiltered=[];
+        foreach($database as $album){
+            if($album['genre']==$genre){
+                $databaseFiltered=$album;
+            }
+        }
+        echo json_encode($databaseFiltered);
+    }else{
+        echo json_encode($database);
+    }
+
+
 ?>

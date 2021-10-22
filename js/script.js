3046da2664449cd01adc7fd53b54cfa6387a101f
manfred-commits <manfredipiraino@gmail.com>
2021-10-22 14:34:38 +0200

    added select genre method to fill select

--- js/script.js
+++ js/script.js
@@ -2,19 +2,37 @@ const app = new Vue({
     el:'#root',
     data:{
         albums:"",
+        selectCategories:[],
     },
     created(){
         axios
-      .get('http://localhost/PHP/php-ajax-dischi/partials/api/index.php')
+      .get('http://localhost/PHP/php-ajax-dischi/partials/api/index.php', {
+            params: {
+            // genre: 'bar'
+            }
+        })
       .then(response => {
         console.log(response.data);
         this.albums=response.data;
+        this.selectFiller();
       })
       .catch(error => {
         console.log(error);
       })
     },
-    method:{
-
+    methods:{
+        selectFiller(){
+            // console.log("ciao");
+            // console.log(this.albums);
+            this.albums.forEach(
+              (element) => {
+                // console.log("ciao");
+                if(!this.selectCategories.includes(element.genre)){
+                    this.selectCategories.push(element.genre);
+                    // console.log("ciao");
+                }
+              }
+            );
+        }
     }
 });

eac8f7dfce484a4ade03d81e21dd20232a06f8ef
manfred-commits <manfredipiraino@gmail.com>
2021-10-22 13:41:50 +0200

    added full $database with genre

--- partials/database.php
+++ partials/database.php
@@ -1,63 +1,73 @@
 <?php
 $database = [
 	[
-		'title' => 'New Jersey',
-		'author' => 'Bon Jovi',
-		'year' => 1988,
-		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+		"poster" =>"https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+		"title" =>"New Jersey",
+		"author" =>"Bon Jovi",
+		"genre" =>"Rock",
+		"year" =>"1988"
 	],
 	[
-		'title' => 'Live at Wembley 86',
-		'author' => 'Queen',
-		'year' => 1992,
-		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
+		"poster" =>"https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+		"title" =>"Live at Wembley 86",
+		"author" =>"Queen",
+		"genre" =>"Pop",
+		"year" =>"1992"
 	],
 	[
-		'title' => 'Ten\'s Summoner\'s Tales',
-		'author' => 'Sting',
-		'year' => 1993,
-		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
+		"poster" =>"https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+		"title" =>"Ten's Summoner's Tales",
+		"author" =>"Sting",
+		"genre" =>"Pop",
+		"year" =>"1993"
 	],
 	[
-		'title' => 'Steve Gadd band',
-		'author' => 'Steve Gadd Band',
-		'year' => 2018,
-		'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
+		"poster" =>"https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+		"title" =>"Steve Gadd Band",
+		"author" =>"Steve Gadd Band",
+		"genre" =>"Jazz",
+		"year" =>"2018"
 	],
 	[
-		'title' => 'Brave new World',
-		'author' => 'Iron Maiden',
-		'year' => 2000,
-		'poster' => 'https://img.discogs.com/ZyRehiDiEs7EsEEKe6PRackT5o4=/fit-in/600x600/filters:strip_icc():format(webp):mode_rgb():quality(90)/discogs-images/R-1355871-1281401669.jpeg.jpg'
+		"poster" =>"https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+		"title" =>"Brave new World",
+		"author" =>"Iron Maiden",
+		"genre" =>"Metal",
+		"year" =>"2000"
 	],
 	[
-		'title' => 'One more car, one more rider',
-		'author' => 'Eric Clapton',
-		'year' => 2002,
-		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+		"poster" =>"https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+		"title" =>"One more car, one more raider",
+		"author" =>"Eric Clapton",
+		"genre" =>"Rock",
+		"year" =>"2002"
 	],
 	[
-		'title' => 'Made in Japan',
-		'author' => 'Deep Purple',
-		'year' => 1972,
-		'poster' => 'https://images.squarespace-cdn.com/content/v1/565c1ab5e4b05079e4bfa169/1484027450966-HZUTF89L0VWTEW97A8Z5/IMG_0023.PNG?format=2500w'
+		"poster" =>"https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+		"title" =>"Made in Japan",
+		"author" =>"Deep Purple",
+		"genre" =>"Rock",
+		"year" =>"1972"
 	],
 	[
-		'title' => 'And justice for all',
-		'author' => 'Metallica',
-		'year' => 1988,
-		'poster' => 'https://img.discogs.com/LaYghp8RiHUDyA3yN26-gVGLeAQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-371162-1531515576-1093.jpeg.jpg'
+		"poster" =>"https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+		"title" =>"And Justice for All",
+		"author" =>"Metallica",
+		"genre" =>"Metal",
+		"year" =>"1988"
 	],
 	[
-		'title' => 'Hard Wired',
-		'author' => 'Dave Weckl',
-		'year' => 1994,
-		'poster' => 'https://img.discogs.com/eAybE8VLCi3EY49kgcP8w0gQkQE=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2173446-1296926502.jpeg.jpg'
+		"poster" =>"https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+		"title" =>"Hard Wired",
+		"author" =>"Dave Weckl",
+		"genre" =>"Jazz",
+		"year" =>"1994"
 	],
 	[
-		'title' => 'Bad',
-		'author' => 'Michael Jackson',
-		'year' => 1987,
-		'poster' => 'https://upload.wikimedia.org/wikipedia/en/5/51/Michael_Jackson_-_Bad.png'
+		"poster" =>"https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+		"title" =>"Bad",
+		"author" =>"Michael Jacjson",
+		"genre" =>"Pop",
+		"year" =>"1987"
 	]
 ];
